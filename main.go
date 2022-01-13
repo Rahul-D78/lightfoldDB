c77e22cb0714f2c2380f16db8d5073a0b46e21f9
Rahul-D78 <rahulparida933@gmail.com>
2022-01-13 09:52:30 +0530

    FEAT: initial version
    Signed-off-by: Rahul-D78 <rahulparida933@gmail.com>
    
    basic functions of lightfoldDB

--- main.go
+++ main.go
@@ -3,8 +3,197 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"os"
+	"path/filepath"
+	"sync"
+
+	"github.com/jcelliott/lumber"
 )
+
+const Version = "1.0.0"
+
+type (
+	Logger interface {
+		Fatal(string, ...interface{})
+		Error(string, ...interface{})
+		Warn(string, ...interface{})
+		Info(string, ...interface{})
+		Debug(string, ...interface{})
+		Trace(string, ...interface{})
+	}
+
+	Driver struct {
+		mutex   sync.Mutex
+		mutexes map[string]*sync.Mutex
+		dir     string
+		log     Logger
+	}
+)
+
+type Options struct {
+	Logger
+}
+
+// Creates a database and returns the Driver we can use to interact with the DB
+func New(dir string, options *Options) (*Driver, error) {
+	dir = filepath.Clean(dir)
+
+	opts := Options{}
+	if options != nil {
+		opts = *options
+	}
+
+	//creating a Logger
+	if opts.Logger == nil {
+		opts.Logger = lumber.NewConsoleLogger(lumber.INFO)
+	}
+
+	//creating a driver of type Driver
+	driver := Driver{
+		dir:     dir,
+		mutexes: make(map[string]*sync.Mutex),
+		log:     opts.Logger,
+	}
+
+	//check if database exists by passing the name of the directory
+	if _, err := os.Stat(dir); err == nil {
+		opts.Logger.Debug("Using '%s' (database already exists)\n", dir)
+		return &driver, nil
+	}
+
+	//database does not exists
+	opts.Logger.Debug("Creating the database at '%s' ...\n", dir)
+	return &driver, os.MkdirAll(dir, 0755)
+
+}
+
+func (d *Driver) Write(collection, resource string, v interface{}) error {
+
+	if collection == "" {
+		return fmt.Errorf("Missing collection!")
+	}
+
+	if resource == "" {
+		return fmt.Errorf("Missing resource!")
+	}
+
+	//only when the write function is completed then only it is unlock otherwise it wont allow anything to work with database
+	mutex := d.getOrCreateMutex(collection)
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	dir := filepath.Join(d.dir, collection)
+	finalPath := filepath.Join(dir, resource+".json")
+	tempPath := finalPath + ".tmp"
+
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return err
+	}
+
+	b, err := json.MarshalIndent(v, "", "\t")
+	if err != nil {
+		return err
+	}
+	b = append(b, byte('\n'))
+
+	if err := ioutil.WriteFile(tempPath, b, 0644); err != nil {
+		return err
+	}
+
+	return os.Rename(tempPath, finalPath)
+}
+
+func (d *Driver) Read(collection, resource string, v interface{}) error {
+	if collection == "" {
+		return fmt.Errorf("Missing collection")
+	}
+
+	if resource == "" {
+		return fmt.Errorf("Missing resource")
+	}
+
+	record := filepath.Join(d.dir, collection, resource)
+
+	if _, err := stat(record); err != nil {
+		return err
+	}
+
+	b, err := ioutil.ReadFile(record + ".json")
+	if err != nil {
+		return err
+	}
+
+	return json.Unmarshal(b, &v)
+}
+
+func (d *Driver) ReadAll(collection string) ([]string, error) {
+	if collection == "" {
+		return nil, fmt.Errorf("Missing collection")
+	}
+
+	dir := filepath.Join(d.dir, collection)
+
+	if _, err := stat(dir); err != nil {
+		return nil, err
+	}
+
+	files, _ := ioutil.ReadDir(dir)
+
+	var records []string
+
+	for _, file := range files {
+		b, err := ioutil.ReadFile(filepath.Join(dir, file.Name()))
+		if err != nil {
+			return nil, err
+		}
+
+		records = append(records, string(b))
+	}
+
+	return records, nil
+}
+
+func (d *Driver) Delete(collection, resource string) error {
+	path := filepath.Join(collection, resource)
+	mutex := d.getOrCreateMutex(collection)
+	mutex.Lock()
+	mutex.Unlock()
+
+	dir := filepath.Join(d.dir, path)
+
+	switch fi, err := stat(dir); {
+	case fi == nil, err != nil:
+		return fmt.Errorf("Unable to find file or directory %v\n", path)
+	case fi.Mode().IsDir():
+		return os.RemoveAll(dir)
+	case fi.Mode().IsRegular():
+		return os.RemoveAll(dir + ".json")
+	}
+	return nil
+}
+
+func (d *Driver) getOrCreateMutex(collection string) *sync.Mutex {
+
+	d.mutex.Lock()
+	defer d.mutex.Unlock()
+
+	m, ok := d.mutexes[collection]
+
+	if !ok {
+		m = &sync.Mutex{}
+		d.mutexes[collection] = m
+	}
+	return m
+}
+
+//checks for files with a .json extension
+func stat(path string) (fi os.FileInfo, err error) {
+	if fi, err = os.Stat(path); os.IsNotExist(err) {
+		fi, err = os.Stat(path + ".json")
+	}
+	return
+}
 
 type Address struct {
 	City string
@@ -25,21 +214,16 @@ func main() {
 
 	dir := "./"
 
-	fmt.Println(os.Geteuid())
-
 	db, err := New(dir, nil)
 	if err != nil {
 		fmt.Println("Error", err)
 	}
 
 	employees := []User{
-		{"DevOps", "12", "42342343", "devops", Address{"not appropriate", "1234"}},
-		{"DevOps", "12", "42342343", "devops", Address{"not appropriate", "1234"}},
-		{"DevOps", "12", "42342343", "devops", Address{"not appropriate", "1234"}},
-		{"DevOps", "12", "42342343", "devops", Address{"not appropriate", "1234"}},
-		{"DevOps", "12", "42342343", "devops", Address{"not appropriate", "1234"}},
-		{"DevOps", "12", "42342343", "devops", Address{"not appropriate", "1234"}},
-		{"DevOps", "12", "42342343", "devops", Address{"not appropriate", "1234"}},
+		{"Linux", "12", "42567567", "linux", Address{"not appropriate", "12"}},
+		{"GoLang", "13", "12232343", "golang", Address{"not appropriate", "34"}},
+		{"K8s", "14", "41432343", "k8s", Address{"not appropriate", "56"}},
+		{"Docker", "15", "89982343", "docker", Address{"not appropriate", "78"}},
 	}
 
 	for _, value := range employees {
@@ -54,7 +238,7 @@ func main() {
 
 	records, err := db.ReadAll("users")
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("Error", err)
 	}
 	fmt.Println(records)
 
@@ -69,11 +253,11 @@ func main() {
 	}
 	fmt.Println(allUsers)
 
-	if err := db.Delete("user", "DevOps"); err != nil {
+	if err := db.Delete("users", "Linux"); err != nil {
 		fmt.Println("Error", err)
 	}
 
-	if err := db.DeleteAll("user", ""); err != nil {
-		fmt.Println("Error", err)
-	}
+	// if err := db.DeleteAll("user", ""); err != nil {
+	// 	fmt.Println("Error", err)
+	// }
 }
